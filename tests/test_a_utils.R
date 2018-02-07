cc3588d3af25165e97956cd4d473012582516fb7
user <no.email>
2018-02-07 10:25:38 +0100

    Adding tests of safeDeparse()

---
+++ tests/test_a_utils.R
@@ -0,0 +1,15 @@
+# test_a_utils.R
+
+library(lmerTestR)
+
+# test safeDeparse() - equivalence and differences to deparse():
+deparse_args <- formals(deparse)
+safeDeparse_args <- formals(lmerTestR:::safeDeparse)
+stopifnot(
+  all.equal(names(deparse_args), names(safeDeparse_args)),
+  all.equal(deparse_args[!names(deparse_args) %in% "width.cutoff"],
+            safeDeparse_args[!names(safeDeparse_args) %in% "width.cutoff"]),
+  all.equal(deparse_args[["width.cutoff"]], 60L),
+  all.equal(safeDeparse_args[["width.cutoff"]], 500L)
+)
+
