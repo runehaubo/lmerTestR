1588c13bdf489cd0a8ed57ad9e75b3ca90d9b0d4
user <no.email>
2018-03-09 14:40:38 +0100

    Installing test for paper and rel to sas

--- tests/test_compare_sas.R
+++ tests/test_compare_sas.R
@@ -29,12 +29,13 @@ stopifnot(
 
 sm <- summary(m.carrots)
 stopifnot(
-  isTRUE(all.equal(sm$coefficients[,"Pr(>|t|)"], c(1e-10, 0.005061, 0.6865554, 0.342613, 0.129157,
-                                            0.088231, 0.846000, 0.354472, 0.526318, 0.020646, 0.010188,
-                                            0.031242, 0.055356, 0.694689, 0.099382, 0.28547,
-                                            0.977774, 0.855653, 0.427737, 0.321086, 0.417465 , 0.204385, 0.784437,
-                                            0.681434, 0.106180, 0.149122, 0.390870, 0.273686), tol=TOL,
-            check.attributes = FALSE))
+  isTRUE(all.equal(sm$coefficients[,"Pr(>|t|)"],
+                   c(1e-10, 0.005061, 0.6865554, 0.342613, 0.129157,
+                     0.088231, 0.846000, 0.354472, 0.526318, 0.020646, 0.010188,
+                     0.031242, 0.055356, 0.694689, 0.099382, 0.28547,
+                     0.977774, 0.855653, 0.427737, 0.321086, 0.417465 , 0.204385, 0.784437,
+                     0.681434, 0.106180, 0.149122, 0.390870, 0.273686), tol=TOL,
+                   check.attributes = FALSE))
 )
 
 # Takes too long to run:
@@ -91,6 +92,63 @@ stopifnot(all.equal(an.1[, "F value"], c(56.5394, 4169.87), tol = TOL),
 
 
 ################################################################################
+# Check exmaple from GLM SAS report
+
+### example from the paper GLM SAS 101 report
+a <- factor(c(1,1,1,2,2,2,2,2,1,2))
+b <- factor(c(1,1,2,1,2,2,2,2,2,1))
+f=factor(c(1,2,1,2,1,2,1,2,1,2))
+y <- c(12,14,11,20,17,23,35,46,15,16)
+dd <- data.frame(a=a, b=b, y=y, f=f)
+
+## check type 2 is order independent
+model <- lmer(y ~ a*b + (1|f), data=dd)
+model2 <- lmer(y ~ b*a + (1|f), data=dd)
+(an <- anova(model, type=2))
+(an2 <- anova(model2, type=2))
+TOL <- 1e-5
+stopifnot(
+  isTRUE(all.equal(an,an2[c(2,1,3),], check.attributes = FALSE, tol=TOL))
+)
+
+## check the results are the same as from SAS proc mixed
+stopifnot(
+  isTRUE(all.equal(an[,"F value"], c(3.90131, 1.32753, 0.99565), tol = 1e-5))
+)
+################################################################################
+## Check type II and III anova tables versus SAS
+
+m.carrots <- lmer(Preference ~ sens2*Homesize
+                  +(1+sens2|Consumer), data=carrots)
+(ancar <- anova(m.carrots, type=2))
+
+stopifnot(
+  isTRUE(all.equal(ancar[,"F value"], c(54.8361, 5.16138, 1.03035), tol = 1e-4))
+)
+
+m <- lmer(Informed.liking ~ Product*Age
+          + (1|Consumer) , data=ham)
+(an <- anova(m, type=2))
+
+stopifnot(
+  isTRUE(all.equal(an[,"F value"], c(2.48135, .005387, 1.48451), tol = 1e-5))
+)
+
+
+fm <- lmer(Preference ~ sens2*Homesize*sens1 + (1|Product),
+           data=carrots)
+(ant2 <- anova(fm, type=2))
+(ant3 <- anova(fm, type=3))
+
+stopifnot(
+  isTRUE(all.equal(ant2[,"F value"],
+                   c(16.4842, 14.0010, .526076, 1.18144,
+                     .107570, .335177, 1.05946), tol = 1e-4)),
+  isTRUE(all.equal(ant3[,"F value"],
+                   c(16.9140, 14.0010,.481148, 1.18144,
+                     .074201, .335177, 1.05946), tol = 1e-4))
+)
+
 ################################################################################
 
 
---
+++ tests/test_lmerTest_paper.R
@@ -0,0 +1,55 @@
+# test_lmerTest_paper.R
+
+library(lmerTest)
+
+# Read in data set
+load(system.file("testdata","test_paper_objects.RData", package="lmerTest"))
+
+# Evaluate code from paper:
+## Section 8.2:
+tv <- lmer(Sharpnessofmovement ~ TVset * Picture + (1 | Assessor) +
+             (1 | Assessor:TVset) + (1 | Assessor:Picture), data = TVbo)
+(an8.2 <- anova(tv))
+(ankr8.2 <- anova(tv, type=2, ddf="Kenward-Roger"))
+
+## Section 8.3:
+m.carrots <- lmer(Preference ~ sens1 + sens2 + (1 + sens1 + sens2 | Consumer) +
+                    (1 | Product), data=carrots)
+(sum8.3 <- coef(summary(m.carrots)))
+
+## Section 8.4:
+tv <- lmer(Sharpnessofmovement ~ TVset * Picture +
+             (1 | Assessor:TVset) + (1 | Assessor:Picture) +
+             (1 | Assessor:Picture:TVset) + (1 | Repeat) + (1 | Repeat:Picture) +
+             (1 | Repeat:TVset) + (1 | Repeat:TVset:Picture) + (1 | Assessor),
+           data = TVbo)
+st <- step(tv)
+names(st)
+(elim_tab_random8.4 <- st$random)
+(elim_tab_fixed8.4 <- st$fixed)
+(an8.4 <- anova(get_model(st)))
+
+## Section 8.5:
+# L <- matrix(0, ncol = 12, nrow = 6)
+# L[1, 7] <- L[2, 8] <- L[3, 9] <- L[4, 10] <- L[5, 11] <- L[6, 12] <- 1
+L <- cbind(array(0, dim=c(6, 6)), diag(6))
+(con1_8.5 <- calcSatterth(tv, L))
+(con2_8.5 <- contest(tv, L))
+
+## Section C:
+# m.carrots <- lmer(Preference ~ sens1 + sens2 + (1 + sens1 + sens2 | Consumer) +
+#                     (1 | product), data = carrots)
+# step(m.carrots, reduce.fixed = FALSE)
+(ran_C <- ranova(m.carrots))
+
+# Compare to validated outputs:
+stopifnot(
+  isTRUE(all.equal(an8.2_save, an8.2, check.attributes = FALSE)),
+  isTRUE(all.equal(ankr8.2_save, ankr8.2, check.attributes = FALSE)),
+  isTRUE(all.equal(sum8.3_save, sum8.3, check.attributes = FALSE)),
+  isTRUE(all.equal(elim_tab_random8.4_save, elim_tab_random8.4, check.attributes = FALSE)),
+  isTRUE(all.equal(elim_tab_fixed8.4_save, elim_tab_fixed8.4, check.attributes = FALSE)),
+  isTRUE(all.equal(an8.4_save, an8.4, check.attributes = FALSE)),
+  isTRUE(all.equal(con1_8.5_save, con1_8.5, check.attributes = FALSE)),
+  isTRUE(all.equal(con2_8.5_save, con2_8.5, check.attributes = FALSE))
+)
