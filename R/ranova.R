cd4c11f648985c40ae52bd4b0f3953ae59e66ddd
user <no.email>
2018-03-23 09:09:10 +0100

    Adding copyright header to R-files

--- R/ranova.R
+++ R/ranova.R
@@ -1,3 +1,24 @@
+#############################################################################
+#    Copyright (c) 2013-2018 Alexandra Kuznetsova, Per Bruun Brockhoff, and
+#    Rune Haubo Bojesen Christensen
+#
+#    This file is part of the lmerTest package for R (*lmerTest*)
+#
+#    *lmerTest* is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    *lmerTest* is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    A copy of the GNU General Public License is available at
+#    <https://www.r-project.org/Licenses/> and/or
+#    <http://www.gnu.org/licenses/>.
+#############################################################################
+#
 # ranova.R - random effects ANOVA table
 
 # ------- Contents: --------
