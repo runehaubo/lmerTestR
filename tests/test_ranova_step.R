54a3d855f857089de1f3e4e98b35d5644c34edbc
Ben Bolker <bbolker@gmail.com>
2020-03-21 20:39:00 -0400

    fix tests etc. for upstream lme4 changes

--- tests/test_ranova_step.R
+++ tests/test_ranova_step.R
@@ -71,7 +71,7 @@ lm2 <- lm(Reaction ~ Days, sleepstudy)
 (an2 <- anova(fm1, lm2))
 stopifnot(
   all.equal(an1[2, "LRT"], an2[2, "Chisq"], tolerance=TOL),
-  all.equal(an1[2, "Df"], an2[2, "Chi Df"], tolerance=TOL),
+  all.equal(an1[2, "Df"], an2[2, "Df"], tolerance=TOL),
   all.equal(an1[1:2, "logLik"], an2[2:1, "logLik"], tolerance=TOL)
 )
 
