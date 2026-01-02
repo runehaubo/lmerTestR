79c55e8b87d2977c5a1fba74c283ab31c9b8ffc5
Rune Haubo B Christensen <rune.haubo@gmail.com>
2026-01-02 10:16:32 +0100

    Fix man-page things

--- R/lmerTest.R
+++ R/lmerTest.R
@@ -102,7 +102,8 @@
 #'   \emph{Journal of Statistical Software}, 82(13), 1--26. doi:10.18637/jss.v082.i13
 #'
 #'
-#' @docType package
+#' @keywords internal 
+"_PACKAGE"
 #' @name lmerTest-package
 #' @aliases lmerTest
 #'
