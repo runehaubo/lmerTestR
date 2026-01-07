39cde9e6b89936450ebb870653aa35bdffe43e77
Rune Haubo B Christensen <rune.haubo@gmail.com>
2026-01-07 13:08:43 +0100

    Adding more tests for evaluation in function + ranova

--- tests/test_ranova_step.R
+++ tests/test_ranova_step.R
@@ -195,6 +195,21 @@ ranova(res) # used to fail - now it works
 data <- iris
 ranova(res) # now it works
 ###########################
+# Evaluation in function without the formula(2):
+# A model with ||-notation:
+f2 <- function(data) {
+  lmer(Reaction ~ Days + (Days||Subject), data)
+}
+res <- f2(sleepstudy)
+ranova(res)
+
+# A model with multiple RE terms:
+f3 <- function(data) {
+  lmer(Coloursaturation ~ TVset*Picture + (1|Assessor:TVset) + (1|Assessor), data)
+}
+res <- f3(TVbo)
+ranova(res)
+###########################
 
 # Check that step works when form is a character vector
 m <- lmer(form, data=ham)
