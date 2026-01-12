1175654698e34b07e78f354eb8a1c6ee96ba0f02
Rune Haubo B Christensen <rune.haubo@gmail.com>
2026-01-12 10:28:12 +0100

    update copyright header

--- R/anova_contrasts.R
+++ R/anova_contrasts.R
@@ -1,5 +1,5 @@
 #############################################################################
-#    Copyright (c) 2013-2020 Alexandra Kuznetsova, Per Bruun Brockhoff, and
+#    Copyright (c) 2013-2026 Alexandra Kuznetsova, Per Bruun Brockhoff, and
 #    Rune Haubo Bojesen Christensen
 #
 #    This file is part of the lmerTest package for R (*lmerTest*)
